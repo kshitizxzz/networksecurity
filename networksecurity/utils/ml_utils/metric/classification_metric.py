4125117c9f987db888544ee690c3170e116b5bf7
kshitizxzz <ms24btech11017@iith.ac.in>
2026-01-17 01:47:32 +0530

    dagshub and ml flow

--- networksecurity/utils/ml_utils/metric/classification_metric.py
+++ networksecurity/utils/ml_utils/metric/classification_metric.py
@@ -1,20 +1,21 @@
 from networksecurity.entity.artifact_entity import ClassificationMetricArtifact
 from networksecurity.exception.exception import NetworkSecurityException
-from sklearn.metrics import f1_score , precision_score, recall_score
-import sys 
-import os
+from sklearn.metrics import f1_score, precision_score, recall_score
+import sys
 
-def get_classification_score(y_true , y_pred)->ClassificationMetricArtifact:
-    try :
-        model_f1_score = f1_score(y_true , y_pred)
-        model_recall_score = recall_score(y_true , y_pred)
-        model_precision_score = precision_score(y_true , y_pred)
+def get_classification_score(y_true, y_pred) -> ClassificationMetricArtifact:
+    try:
+        f1 = f1_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
 
-        classification_metric = ClassificationMetricArtifact(model_f1_score = f1_score , 
-                                                             model_recall_score = recall_score,
-                                                             model_precision_score = precision_score
-                                                            )
+        classification_metric = ClassificationMetricArtifact(
+            f1_score=f1,
+            precision_score=precision,
+            recall_score=recall
+        )
+
         return classification_metric
-    
-    except Exception as e :
-        raise NetworkSecurityException(e,sys)
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
