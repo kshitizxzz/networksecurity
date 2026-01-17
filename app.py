cd15c160509122edd4d6f623075a3074815f3573
kshitizxzz <ms24btech11017@iith.ac.in>
2026-01-17 18:03:35 +0530

    output is done

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 import pandas as pd
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 
 
 from networksecurity.utils.main_utils.utils import load_object
@@ -53,7 +54,30 @@ async def train_route():
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+    
+@app.post("/predict")
+async def predict_route (request: Request, file: UploadFile=File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        #print(df)
+        preprocesor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        #df['predicted_column').replace(-1, 0)
+        #return df.to_json()
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
+        return templates. TemplateResponse("table.html", {"request": request, "table": table_html})
+    
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
 
     
-if __name__ == '__main__':
-    app_run(app , host='localhost' , port = 8000)
+# if __name__ == '__main__':
+#     app_run(app , host='localhost' , port = 8000)
