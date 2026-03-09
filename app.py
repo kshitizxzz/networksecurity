afa4bfb00b4221ff1ee4aa938e4d6c5b3ac8a152
kshitizxzz <ms24btech11017@iith.ac.in>
2026-03-10 00:54:14 +0530

    done

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ async def predict_route (request: Request, file: UploadFile=File(...)):
         raise NetworkSecurityException(e,sys)
 
     
-# if __name__ == '__main__':
-#     app_run(app , host='localhost' , port = 8000)
+if __name__ == '__main__':
+    app_run(app , host='localhost' , port = 8000)
